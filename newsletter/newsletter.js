21df877b370f09828366f4134070918aecc264db
Isaac D2 <hi@isaacd2.com>
2025-12-08 16:34:45 -0500

    moved toast messages to their own file

--- newsletter/newsletter.js
+++ newsletter/newsletter.js
@@ -375,19 +375,6 @@ async function broadcastEmail(event) {
     return false;
   }
 }
-
-function toastMessage(text, success = true) {
-  // Get the snackbar DIV
-  var x = document.getElementById("snackbar");
-
-  x.textContent = text;
-  // Add the "show" class to DIV
-  x.className = success ? "showSuccess" : "showError";
-
-  // After 3 seconds, remove the show class from DIV
-  setTimeout(function(){ x.className = x.className.replace(success ? "showSuccess" : "showError", ""); }, 3999);
-}
-
 //#endregion
 
 //#region EVENT LISTENERS
