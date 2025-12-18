c0c81e24122a062153f770c12e28d20f4019fe7f
Isaac D2 <hi@isaacd2.com>
2025-12-18 15:56:15 -0500

    added draggable sizing and individual sending

--- newsletter/newsletter.js
+++ newsletter/newsletter.js
@@ -6,11 +6,17 @@ let newsletter = null;
 let authRetried = false;
 let form;
 let backgroundImageform;
-var modal = document.getElementById("newsletterSettingsModal");
+var newsletterSettingsModal = document.getElementById("newsletterSettingsModal");
+var singleSendModal = document.getElementById("singleSendModal");
 var preview = document.getElementById("previewContainer");
 let livePreview = false;
 let template = null;
 const previewWindow = document.querySelector("iframe").contentWindow;
+var splitInstance = Split(['#split-0'], {
+    minSize: 500,
+    gutterSize: 16,
+});
+let profile;
 
 // Allows for production and development switching
 const version = getAPIMode();
@@ -38,7 +44,8 @@ function getStats() {
     })
     .then(res => {
         if (res.status === 401) {
-            retry();
+            retry(getStats.name);
+
         }
         if (!res.ok) throw new Error("Failed to load stats");
         return res.json();
@@ -65,12 +72,13 @@ async function loadNewsletterData(newsletterId) {
     });
 
     if (response.status === 401) {
-      retry();
+      retry(loadNewsletterData.name);
     } if (!response.ok) {
       throw new Error(`Failed to load newsletter: ${response.status}`);
     }
 
     const newsletter = await response.json();
+    console.log(newsletter);
 
   setNewsletterDetails(newsletter);
 
@@ -83,12 +91,18 @@ async function loadNewsletterData(newsletterId) {
 }
 
 function setNewsletterDetails(newsletter) {
+  console.log(newsletter);
   document.getElementById('subject').value = newsletter.subject || "Untitled";
   document.getElementById('preview').value = newsletter.preview || "";
   newsletterData = newsletter.content || "";
   document.getElementById('sendDate').value = newsletter.sendDate || "";
   document.getElementById('pageTitle').textContent = "Edit Newsletter";
-  document.getElementById('backgroundColor').value = newsletter.backgroundColor.substring(0,7);
+  let backgroundColor = document.getElementById('backgroundColor').value
+  if (newsletter.backgroundColor) {
+    backgroundColor = newsletter.backgroundColor.substring(0,7);
+  } else {
+    backgroundColor = "#000000"
+  }
 
   if (trixInitialized) {
     loadTrixContent(newsletterData);
@@ -127,7 +141,7 @@ async function getUploadURL() {
         });
         
         if (response.status === 401) {
-            retry();
+            retry(getUploadURL.name);
         } if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         } else {
@@ -238,7 +252,8 @@ async function saveNewsletter(event) {
     );
 
     if (response.status === 401) {
-      retry();
+      console.log(saveNewsletter.name);
+      retry(saveNewsletter.name);
     } if (!response.ok) {
       throw new Error("Failed to save newsletter");
     }
@@ -292,7 +307,7 @@ async function updateSettings() {
     );
 
     if (response.status === 401) {
-      retry();
+      retry(updateSettings.name);
     } if (!response.ok) {
       throw new Error("Failed to update settings");
     }
@@ -337,7 +352,7 @@ async function updateBackgroundImage(event) {
     );
 
     if (response.status === 401) {
-      retry();
+      retry(updateBackgroundImage.name);
     } if (!response.ok) {
       throw new Error("Failed to update background image");
     }
@@ -352,7 +367,7 @@ async function updateBackgroundImage(event) {
   }
 }
 
-async function sendPreviewEmail(event) {
+async function sendPreviewEmail(event, previewEmail = true, emailAddress = null, userId = null) {
   await saveNewsletter(event);
   token = localStorage.getItem("id_token");
 
@@ -365,12 +380,14 @@ async function sendPreviewEmail(event) {
           },
           body: JSON.stringify({
               newsletterId: newsletterId,
-              previewEmail: true
+              previewEmail: previewEmail,
+              emailAddress: emailAddress,
+              userId: userId
           })
       });
 
       if (response.status === 401) {
-          retry();
+          retry(sendPreviewEmail.name);
       } if (response.ok) {
           return true;
       } else {
@@ -414,7 +431,7 @@ async function broadcastEmail(event) {
     );
 
     if (response.status === 401) {
-      retry();
+      retry(broadcastEmail.name);
     } if (!response.ok) {
       const message = "Failed to send newsletter";
       throw new Error(message);
@@ -444,7 +461,7 @@ async function deleteNewsletter() {
     );
 
     if (response.status === 401) {
-      retry();
+      retry(deleteNewsletter.name);
     } if (!response.ok) {
       throw new Error("Failed to delete newsletter");
     }
@@ -457,6 +474,7 @@ async function deleteNewsletter() {
 }
 
 async function getTemplate() {
+    console.log(newsletter);
     try {
       const version = getAPIMode();
       tooken = localStorage.getItem("id_token");
@@ -472,7 +490,7 @@ async function getTemplate() {
       );
 
       if (templateReponse.status === 401) {
-        retry();
+        retry(getTemplate.name);
       } if (!templateReponse.ok) {
         throw new Error("Failed to get template details");
       }
@@ -505,15 +523,97 @@ function safeSubstitute(templateString, data) {
 }
 
 async function updatePreview(templateString) {
-    const content = document.getElementById("content").value;
+    let content = document.getElementById("content").value;
+    content = adjustContentProperties(content);
     const data = {
         tracking_url: "",
+        preview: newsletter.preview,
         content: content,
-        businessAddress: "8561 Gander Creek Dr",
-        backgroundImageUrl: newsletter.backgroundImageUrl
+        businessAddress: "Address not available in preview only",
+        backgroundImageUrl: newsletter.backgroundImageUrl,
+        backgroundColor: newsletter.backgroundColor
     };
     const previewData = safeSubstitute(templateString, data);
     previewWindow.postMessage(previewData);
+}
+
+function adjustContentProperties(content) {
+  // Replace H1 Tags with H2 Tags
+  content = content.replace(/<h1(\s*[^>]*)>/g, "<h2$1>");
+  content = content.replace(/<\/h1>/g, "</h2>");
+  
+  // Make images fit again
+  const pattern = /<img(?<attrs>.*?)>/gs;
+
+  function replacer(match, attrs) {
+    // Remove existing width and height attributes
+    let modifiedAttrs = attrs.replace(/width="\d+"/gi, '');
+    modifiedAttrs = modifiedAttrs.replace(/height="\d+"/gi, '');
+
+    // The desired inline style
+    const newStyle = 'width: 500px; height: auto;';
+
+    // Check if an existing style attribute exists
+    if (/style="[^"]*"/gi.test(modifiedAttrs)) {
+      // Append the new style to the existing style
+      modifiedAttrs = modifiedAttrs.replace(
+        /style="([^"]*)"/gi,
+        (match, existingStyles) => {
+          // Ensure space separation and prevent duplicate styles
+          const combinedStyle = `${existingStyles.trim()} ${newStyle.trim()}`.trim();
+          return `style="${combinedStyle}"`;
+        }
+      );
+    } else {
+      // If no existing style attribute, add a new one
+      modifiedAttrs += ` style="${newStyle}"`;
+    }
+
+    // Return the modified image tag
+    return `<img${modifiedAttrs}>`;
+  }
+  
+  content = content.replace(pattern, replacer);
+  
+  // Center Captions
+    // I can uncomment this in the future. I need to figure out how to make this part work on the backend first.
+    // content = content.replace(/attachment__caption/g, 'attachment__caption\" style=\"text-align: center;');
+
+  return content;
+}
+
+function resizeImages(html) {
+  const pattern = /<img(?<attrs>.*?)>/gs;
+
+  function replacer(match, attrs) {
+    // Remove existing width and height attributes
+    let modifiedAttrs = attrs.replace(/width="\d+"/gi, '');
+    modifiedAttrs = modifiedAttrs.replace(/height="\d+"/gi, '');
+
+    // The desired inline style
+    const newStyle = 'width: 500px; height: auto;';
+
+    // Check if an existing style attribute exists
+    if (/style="[^"]*"/gi.test(modifiedAttrs)) {
+      // Append the new style to the existing style
+      modifiedAttrs = modifiedAttrs.replace(
+        /style="([^"]*)"/gi,
+        (match, existingStyles) => {
+          // Ensure space separation and prevent duplicate styles
+          const combinedStyle = `${existingStyles.trim()} ${newStyle.trim()}`.trim();
+          return `style="${combinedStyle}"`;
+        }
+      );
+    } else {
+      // If no existing style attribute, add a new one
+      modifiedAttrs += ` style="${newStyle}"`;
+    }
+
+    // Return the modified image tag
+    return `<img${modifiedAttrs}>`;
+  }
+
+  return html.replace(pattern, replacer);
 }
 
 async function populateNewsletterTemplateDropdown() {
@@ -530,7 +630,7 @@ async function populateNewsletterTemplateDropdown() {
         });
 
         if (response.status === 401) {
-            retry();
+            retry(populateNewsletterTemplateDropdown.name);
         } if (!response.ok) {
             const message = "Failed to load newsletter template list"
             throw new Error(message);
@@ -563,6 +663,108 @@ async function populateNewsletterTemplateDropdown() {
     }
 }
 
+function setSplit(preview = false) {  
+  if (preview) {
+    splitInstance = Split(['#split-0', '#split-1'], {
+      minSize: [500, 100],
+      gutterSize: 16,
+    });
+    document.getElementById('split-1').style.display = "flex";
+    document.getElementById('newsletterStats').style.display = "none";
+    document.getElementById('formSubject').style.display = "none";
+    document.getElementById('formPreview').style.display = "none";
+    document.getElementById('livePreviewNewsletter').textContent = "Hide Live Preview";
+  } else {
+    splitInstance.destroy(preserveStyles = true);
+    splitInstance = Split(['#split-0'], {
+                        minSize: 500,
+                        gutterSize: 16,
+                    });
+    document.getElementById('split-1').style.display = "none";
+    document.getElementById('newsletterStats').style.display = "flex";
+    document.getElementById('formSubject').style.display = "block";
+    document.getElementById('formPreview').style.display = "block";
+    document.getElementById('livePreviewNewsletter').textContent = "Show Live Preview";
+  }
+}
+
+async function getProfileDetails() {
+    const version = getAPIMode();
+    token = localStorage.getItem("id_token");
+    try {
+        const response = await fetch(`https://api.dinod2.com/${version}/profile`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: token
+            }
+        });
+
+        if (response.status === 401) {
+        retry(getProfileDetails.name);
+        } if (!response.ok) {
+        throw new Error(`Failed to load newsletter: ${response.status}`);
+        }
+
+        const profile = await response.json();
+
+        populateSubscriberListDropdown(profile);
+
+        return profile;
+    } catch (err) {
+        toastMessage("Error loading profile. Please refresh.", false);
+        console.error("Error loading profile:", err);
+        return null;
+    }
+}
+
+async function populateSubscriberListDropdown(profile) {
+    const userDropdown = document.getElementById("subscriberListDropdown");
+    const version = getAPIMode();
+    token = localStorage.getItem("id_token");
+    try {
+        const response = await fetch(`https://api.dinod2.com/${version}/subscribers`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: token
+            }
+        });
+
+        if (response.status === 401) {
+            retry(populateSubscriberListDropdown.name);
+        } if (!response.ok) {
+            const message = "Failed to load subscriber list"
+            throw new Error(message);
+        }
+
+        const subscribers = await response.json();
+        userDropdown.innerHTML = "";
+        let foundActive = false;
+        let previewEmailValue = null;
+        subscribers.forEach(sub => {
+            if (sub.condition == "Subscribed") {
+                foundActive = true;
+                const opt = document.createElement("option");
+                opt.value = JSON.stringify({ id: sub.id, email: sub.emailAddress });
+                opt.textContent = `${sub.firstName} (${sub.emailAddress})`;
+                if (profile.previewEmailId === sub.id) {
+                    previewEmailValue = opt.value;
+                }
+                userDropdown.appendChild(opt);
+            }
+        });
+        userDropdown.value = previewEmailValue;
+
+        if (!foundActive) {
+            userDropdown.innerHTML = '<option value="">No active users</option>';
+        }
+    } catch (e) {
+        console.error(e);
+        userDropdown.innerHTML = '<option value="">Error loading users</option>';
+    }
+}
+
 //#endregion
 
 //#region EVENT LISTENERS
@@ -583,6 +785,7 @@ window.addEventListener("authReady", async (e) => {
         getStats();
         getTemplate();
         populateNewsletterTemplateDropdown();
+        profile = getProfileDetails();
 
         if (form) {
           form.addEventListener("submit", saveNewsletter);
@@ -593,6 +796,11 @@ window.addEventListener("authReady", async (e) => {
             handleTrixInitialize({ target: existingEditor });
         }
     }
+});
+
+window.addEventListener("authIsRetried", async (e) => {
+    const theName = e.detail.name;
+    window[theName](e);
 });
 
 // Upload image when added to Trix
@@ -608,8 +816,12 @@ addEventListener("trix-change", () => {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == newsletterSettingsModal) {
+    newsletterSettingsModal.style.display = "none";
+  }
+
+  if (event.target == singleSendModal) {
+    newsletterSettingsModal.style.display = "none";
   }
 } 
 
@@ -626,37 +838,67 @@ document.getElementById('backBtn').onclick = () => {
 };
 
 document.getElementById("changeSettings").onclick = () => {
-  modal.style.display = "block";
+  newsletterSettingsModal.style.display = "block";
 };
 
 document.getElementById("submitSettings").onclick = async => {
   updateSettings();
-  modal.style.display = "none";
+  newsletterSettingsModal.style.display = "none";
 }
 
 document.getElementsByClassName("close")[0].onclick = () => {
-  modal.style.display = "none";
+  newsletterSettingsModal.style.display = "none";
+  singleSendModal.style.display = "none";
 }
 
-document.getElementById("previewNewsletter").onclick = async (event) => {
-  const success = await sendPreviewEmail(event);
+document.getElementById("sendToOne").onclick = () => {
+  singleSendModal.style.display = "block";
+};
 
-  posthog.capture('newsletter_previewed', {
+document.getElementById("submitSend").onclick = async (event) => {
+  const selectedSubscriberEmail = JSON.parse(document.getElementById('subscriberListDropdown').value);
+  singleSendModal.style.display = "none";
+  const success = await sendPreviewEmail(event, false, selectedSubscriberEmail.email, selectedSubscriberEmail.id);
+  
+  posthog.capture('newsletter_single_send', {
     newsletterId: newsletterId,
+    recipientEmail: selectedSubscriberEmail.email,
+    recipientUserId: selectedSubscriberEmail.id,
     successful: success
   });
 
   if (success) {
-    toastMessage("Preview email sent", true);
+    toastMessage(`Email sent to ${selectedSubscriberEmail.email}`, true);
   } else {
-    toastMessage("Failed to send preview", false);
+    toastMessage("Failed to send email", false);
   }
+}
+
+document.getElementById("previewNewsletter").onclick = async (event) => {
+  if (confirm("You are sending an email to yourself to preview.\nAre you sure you want to continue?")) {
+    const success = await sendPreviewEmail(event);
+
+    posthog.capture('newsletter_previewed', {
+      newsletterId: newsletterId,
+      successful: success
+    });
+
+    if (success) {
+      toastMessage("Preview email sent", true);
+    } else {
+      toastMessage("Failed to send preview", false);
+    }
+  }
+};
+
+document.getElementById("sendToOne").onclick = async (event) => {
+  singleSendModal.style.display = "block";
 };
 
 document.getElementById("livePreviewNewsletter").onclick = async (event) => {
   livePreview = livePreview ? false : true;
   livePreview ? getTemplate() : null;
-  preview.style.display = livePreview ? "flex" : "none";
+  livePreview ? setSplit(true) : setSplit(false);
   updatePreview(template);
 }
 
